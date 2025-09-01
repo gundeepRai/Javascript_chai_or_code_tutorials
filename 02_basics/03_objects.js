f330992ac79be47817bfa708a4e90f1dfbb5478b
Gundeep Rai <140233811+gundeepRai@users.noreply.github.com>
2025-09-01 18:33:12 +0000

    un complete lectures

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,10 @@
+// singleton
+
+// object literals
+const JsUser = {
+    name: "Shri ji",
+    age: 10,
+    email: "laalji@braj.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Tues"]
+}
