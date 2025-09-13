d9004b3cccba82a9c210fa0f458932ec98035994
Gundeep Rai <140233811+gundeepRai@users.noreply.github.com>
2025-09-13 12:55:08 +0000

    upto vd-22

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,14 +1,10 @@
+// -------------------------- vd-19 --------------------------------------------
 // if a fucntion's name is "eg_func"
 // ' eg_func ' -> carries the reference of that function
 // ' eg_func() ' -> does the execution of function, parentheses lagate hi execution
 
-
-
-
-
-
-
-
+// "Arguments": values which are passed during function calling
+// "Parameters": are the inputs accepted in functions definition
 
 
 
--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -16,7 +16,7 @@
 
 //  ------------------------- vd-22 ---------------------------------
 
-one(); 
+// one(); 
 
 {function one(){
     const username = "Shri ji"
@@ -32,4 +32,24 @@ one();
     two();
 }}
 
-one();
+// one();
+
+// closure in js : A closure in JavaScript is the combination of a function and the lexical environment within which that function was declared. This means that an inner function, defined inside an outer function, retains access to the outer function's variables and scope, even after the outer function has finished executing. 
+
+
+
+//  ******************************* interesting *****************************
+addOne(4); //can call before defining/initialization, perfectly fine
+function addOne(num){
+    return num+1
+}
+addOne(5);
+
+// addTwo(5); calling addTwo expression before defining/initialization gives error
+const addTwo = function(num){
+    return num+2;
+}
+addTwo(5);
+
+// the "addOne" function is simply a function 
+// while the "addTwo" function is known as an "Expression"
